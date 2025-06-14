0d0ef7d45e0f55f9a4722c8f9889157be5bab985
Chopda Armik <armikchopda35280@gmail.com>
2025-06-14 11:25:32 +0000

    datatype summary

--- basics/new.js
+++ basics/new.js
@@ -3,3 +3,25 @@ console.log("1" <= 1)
 console.log(null == 0)
 console.log(null <= 0)
 console.log(null <= 0)
+
+const id=Symbol('123')
+const d2=Symbol('123')
+console.log(id == d2)
+const bigNumber=1234567890009876n
+console.log(typeof bigNumber)
+
+
+// Array 
+
+const array=['armik','dhruv','jeel']
+let myData={
+    name:'armik',
+    age:12,
+    email:'armik@gmail.com'
+
+}
+console.log(typeof myData)
+function armik(){
+    console.log('hello world')
+}
+armik()
