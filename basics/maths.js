24785b7e2a7cda3dce7d9af6cba60c44fc3db95c
Chopda Armik <armikchopda35280@gmail.com>
2025-06-16 03:54:11 +0000

    math or number

---
+++ basics/maths.js
@@ -0,0 +1,15 @@
+console.log(Math.abs(-4))
+console.log(Math.abs(4))
+console.log(Math.round(2.3))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(8.9))
+console.log(Math.sqrt(9))
+console.log(Math.max(1,2,3,4,5,6))
+console.log(Math.min(1,4,2,4,2,7,8,9))
+
+
+// console.log((Math.random()*10).toPrecision(1))
+
+const min=0
+const max=20
+console.log(Math.floor(Math.random(min,max)*(max-min)))
---
+++ basics/number.js
@@ -0,0 +1,9 @@
+
+const score=100
+const onatherNumber= new Number(100000)
+console.log(onatherNumber);
+console.log(onatherNumber.toFixed(2))
+console.log(onatherNumber.toString().length)
+console.log(onatherNumber.toPrecision(7))
+console.log(onatherNumber.toLocaleString('en-IN'))
+
