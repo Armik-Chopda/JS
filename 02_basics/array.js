cc5c748888ee1cf94120f7e6b668223db6d443a6
Chopda Armik <armikchopda35280@gmail.com>
2025-06-18 02:41:38 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,20 @@
+const myArray=[1,2,3,4,5,6]
+console.log(myArray)
+console.log(myArray.slice(1,4))
+
+console.log(myArray.splice(1,3))
+console.log(myArray)
+
+const array2=new Array(12,45,67,34,56,43,78)//remove and add element form last
+array2.push(192)
+array2.pop()
+
+console.log(array2);
+
+array2.unshift(11)    // remove and add element from the first and 
+console.log(array2)
+
+array2.shift()
+console.log(array2);
+
+
