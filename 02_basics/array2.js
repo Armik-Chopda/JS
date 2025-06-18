2705a0b2eebdab12b6754a498c02ca7fe5ea4a44
Chopda Armik <armikchopda35280@gmail.com>
2025-06-18 03:58:39 +0000

    ojects

---
+++ 02_basics/array2.js
@@ -0,0 +1,19 @@
+const m1=['thor','Avengers','Captain America.',
+'Spider-Man.',
+'Deadpool.',
+'Black Panther.']
+
+const cd=['batman','superman','robin',]
+const newArray1=m1.concat(cd)
+console.log(newArray1)
+console.log(Array.isArray('armik'))
+console.log(Array.from('armik'));
+
+
+// marge variable into array
+let a=1
+let b=2
+let c=3
+console.log(Array.of(a,c,c));
+
+
