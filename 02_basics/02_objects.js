1bbb8f7f6124aa6588a7b522bf65f90478201fd1
Chopda Armik <armikchopda35280@gmail.com>
2025-06-19 03:05:05 +0000

    object

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -4,7 +4,7 @@ faceBookUser.id='001';
 faceBookUser.name='armik';
 faceBookUser.age=19;
 faceBookUser.isLoggedIn=true
-console.log(faceBookUser)
+// console.log(faceBookUser)
 
 const newObject={                           // use mutipal key inside the key or funcrions also
     email:'armik@gmail.com',
@@ -19,17 +19,50 @@ const newObject={                           // use mutipal key inside the key or
         }
     }
 }
-console.log(newObject.fullName.userName.finalName.finalName);//for access the element from the object
+// console.log(newObject.fullName.userName.finalName.finalName);   //for access the element from the object
 
 
 //marge tow object
 
 const obj=Object.assign({},faceBookUser,newObject)
-console.log(obj);
+// console.log(obj);
 
 // 2 method to marge tow Object
 
 const obj2={...faceBookUser,...newObject}
-console.log(obj2);
+// console.log(obj2);
+
+users=[{id:'001',
+    name:'armik'
+},
+{id:'001',
+    name:'armik'
+},
+{id:'001',
+    name:'armik'
+},
+
+
+
+]
+// console.log(users[1].id);     // access object or elements from array of ojects
+
+// console.log(Object.keys(faceBookUser));    //access keys of oject
+// console.log(Object.values(faceBookUser));   //access values of object
+// console.log(faceBookUser.hasOwnProperty('age'));  // check a object has this.. key or not
+
+
+const obj3={
+    name:'armik',
+    email:'armik@gamil.com'
+}
+obj3.name    
+//or
+const {name}=obj3    //<-  d-stracture  object
+const {name:n}=obj3
+console.log(name);
+console.log(n);
+
+
 
 
