ff630a3816dbc56c77f0f8ab241e876e212edfad
Chopda Armik <armikchopda35280@gmail.com>
2025-06-20 02:04:31 +0000

    condition

---
+++ control_flow/condition.js
@@ -0,0 +1,34 @@
+const userLogdeIn=true
+if(userLogdeIn){
+    console.log('Yes , User logdein');
+    
+}
+else{
+    console.log('no now');
+    
+}
+const month=2
+switch (month) {
+    case 1:
+        console.log('jan');
+        break;
+    case 2:
+        console.log('feb');
+        break;
+    case 3:
+        console.log('mar');
+        break;
+    case 4:
+        console.log('april');
+        break
+    case 5:
+        console.log('may');
+        break;
+    case 36:
+        console.log('jun');
+        break;
+
+    default:
+        console.log('invalid!')
+        break;
+}
