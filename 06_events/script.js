418f6d30924ed176e4456f45c34b5cacbb4b3128
Chopda Armik <armikchopda35280@gmail.com>
2025-06-24 03:33:25 +0000

    events

---
+++ 06_events/script.js
@@ -0,0 +1,24 @@
+// document.getElementById('images').addEventListener('click',(e)=>{
+//     alert('element  clicked! inside the ul')
+// },true)
+// document.getElementById('owl').addEventListener('click',function(e){
+//     alert('owl cliked!')
+// },true)
+// document.getElementById('google').addEventListener('click',function(e){
+//     console.log('Google clickd')
+//     e.stopPropagation()
+//     e.preventDefault()
+// })
+
+document.getElementById('images').addEventListener('click',function(event){
+    event.stopPropagation();
+    event.preventDefault()
+    if (event.target.tagName==='IMG','A') {
+        const removeIt= event.target.parentNode
+    removeIt.parentNode.removeChild(removeIt)
+        
+    }
+    
+    
+   
+})
